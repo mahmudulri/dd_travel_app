ce7ad231ebc226ac8819892f472901d666d082dd
MD. Mahmudul Hasan <61178414+mahmudulri@users.noreply.github.com>
2023-01-31 10:51:12 +0600

    adddd

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dd_travel_app/screens/bottomnav/bottomnav.dart';
 import 'package:dd_travel_app/screens/draft.dart';
 import 'package:dd_travel_app/screens/auth/getstarted_screen.dart';
+import 'package:dd_travel_app/screens/explore.dart';
 import 'package:dd_travel_app/screens/homescreen/home_screen.dart';
 import 'package:dd_travel_app/screens/auth/login_screen.dart';
 import 'package:dd_travel_app/screens/profilescreen/profile_screen.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       ),
       // initialRoute: splashScreen,
       // getPages: getPages,
-      home: BottomNavScreen(),
+      home: ExploreScreen(),
     );
   }
 }
--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,13 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Text("Explore"),
+      backgroundColor: Colors.transparent,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/banner1.png",
+              width: screenWidth,
+              fit: BoxFit.cover,
+              height: screenHeight * 0.350,
+            ),
+            SafeArea(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: screenHeight * 0.045,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Where do\nyou wanna go ?",
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w500,
+                            fontSize: screenHeight * 0.030,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(
+                                    width: 1,
+                                    color: Colors.white,
+                                  )),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Icon(
+                                  Icons.wb_cloudy_outlined,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Rainy',
+                              style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.150,
+                  ),
+                  Container(
+                    height: screenHeight,
+                    width: screenWidth,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
